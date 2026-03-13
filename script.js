35619dfc4f12937b7810d532023a3986744ea735
NIKHIL KUMAR <nikhil.fut.dev@gmail.com>
2026-03-14 01:57:02 +0530

    .vscode/settings.json modified:   html_notes.html modified:   script.js

--- script.js
+++ script.js
@@ -24,4 +24,11 @@ ba.addEventListener("mousemove", (e) => {
   let x = e.offsetX;
   ba.querySelector(".after").style.width = x + "px";
   slider.style.left = x + "px";
-});  
+});
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
